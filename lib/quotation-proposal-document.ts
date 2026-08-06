b3f05d328388f087877d6c8e6071d4bc36000922
AMAN28082001 <amankrrajak288@gmail.com>
2026-08-06 14:10:26 +0530

    Update lightning arrestor specification details in quotation proposal document

--- lib/quotation-proposal-document.ts
+++ lib/quotation-proposal-document.ts
@@ -549,8 +549,8 @@ export function buildSpecRows(products: ProductSelection | ProductsLike): SpecRo
     },
     {
       component: "Lightning Arrestor & Earthing",
-      specification: "Lightning Protection, Green Earthing Wire",
-      brandModel: "2-3 Meter LA & JMP Green Earthing Wire",
+      specification: "Copper-bonded lightning protection with green earthing wire",
+      brandModel: "JMP Green Earthing Wire",
       qty: "1 Set",
     },
     {
